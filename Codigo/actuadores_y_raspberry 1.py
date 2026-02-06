3257927691d118e4676e6fa6c35e75d6cfd15adc
Frena2 <144627173+Frena2@users.noreply.github.com>
2026-02-06 14:10:51 +0100

    Introducimos clave
    Creamos la clave 1234 y pedimos al usuario que introduzca la clave para desactivar la alarma.

--- Codigo/actuadores_y_raspberry 1.py
+++ Codigo/actuadores_y_raspberry 1.py
@@ -1,19 +1,20 @@
-##Importamos la libreria GPIO que nos permite controlar los puertos GPIO y la librería de tiempo
+##Importamos la libreria GPIO que nos permite controlar los puertos GPIO
 import RPi.GPIO as GPIO
 import time
-
 
 ##Definimos el modo BCM que nos permite utilizar los numeros de pines GPIO
 GPIO.setmode(GPIO.BCM)
 
-##Definimos los pines de entrada y salida
+##Definimos pines de entrada y salida
 GPIO.setup(17, GPIO.OUT)  #LED Amarillo
-GPIO.setup(27, GPIO.IN)   #Sensor de movimiento
+GPIO.setup(27, GPIO.IN)  #Sensor de movimiento
 GPIO.setup(22, GPIO.OUT)  #LED Rojo
 GPIO.setup(10, GPIO.OUT)  #Zumbador
 
 
+
 mov=0
+clave = 1234
 
 while True :
 	if GPIO.input(27) == True:
@@ -26,17 +27,27 @@ while True :
 
 	if mov == 1:
 		GPIO.output(17, True)
-		time.sleep(1)
-	
+		#time.sleep(1)
 	elif mov == 2:
 		GPIO.output(17, False)
 		GPIO.output(22, True)
-		time.sleep(1)
-		
+		#time.sleep(1)
 	elif mov == 3:
 		GPIO.output(22, False)
 		GPIO.output(10, True)
 		time.sleep(5)
-		
+		contraseña =int(input("Introduce la clave para desactivar la alarma: "))
 
+		if clave != contraseña :
+			print ("AVISANDO A LA POLICIA")
+			mov = 0
 
+		elif clave == 1234 and clave == contraseña :
+			print ("ALARMA DESACTIVADA")
+			GPIO.output(10, False)
+			mov=0
+			
+		else  :
+			print ("ALARMA DESACTIVADA")
+			GPIO.output(10, False)
+			mov=0

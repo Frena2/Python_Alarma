acbcc401a852d3be67ea5a4fa96e64298fd6bb6c
Frena2 <144627173+Frena2@users.noreply.github.com>
2026-02-10 18:42:25 +0100

    Limpieza
    Limpieza de código y las descripciones para que no haya cosas inútiles o explicaciones erroneas.

--- Codigo/actuadores_y_raspberry 1.py
+++ Codigo/actuadores_y_raspberry 1.py
@@ -1,12 +1,13 @@
 ##Importamos la libreria GPIO que nos permite controlar los puertos GPIO
 import RPi.GPIO as GPIO
-
-
-##Definimos el modo BCM que nos permite utilizar los numeros de pines GPIO
-GPIO.setmode(GPIO.BCM)
 import time
 
-##Definimos el pin GPIO 17 como salida (LED) y el 27 como entrada (PULSADOR/SENSOR)
+
+##Definimos el modo BCM que nos permite utilizar los números de pines GPIO
+GPIO.setmode(GPIO.BCM)
+
+
+##Definimos los pines de entrada y salida
 GPIO.setup(17, GPIO.OUT)  #LED Amarillo
 GPIO.setup(27, GPIO.IN)  #Sensor de movimiento
 GPIO.setup(22, GPIO.OUT)  #LED Rojo
@@ -28,26 +29,25 @@ try:
     
     	if mov == 1:
     		GPIO.output(17, True)
-    		#time.sleep(1)
     	elif mov == 2:
     		GPIO.output(17, False)
     		GPIO.output(22, True)
-    		#time.sleep(1)
     	elif mov == 3:
     		GPIO.output(22, False)
     		GPIO.output(10, True)
     		time.sleep(5)
+
     		contraseña =input("Introduce la clave para desactivar la alarma: ")
     
     		if clave != contraseña :
     			print ("AVISANDO A LA POLICIA")
-    			
     			mov = 0
     		elif clave == str(1234) and clave == contraseña :
     			print ("ALARMA DESACTIVADA")
     			GPIO.output(10, False)
     			mov=0
     			print ("Debe cambiar la clave")
+
     			while len(clave) < 6 or clave.isalnum() == True :
     				clave=input("Defina la nueva clave: ")
     				if len(clave) < 6 :
